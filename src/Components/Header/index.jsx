41efe33989938590ea7ee018ed99c80c39a79643
João victor <joaooleodoro@gmail.com>
2024-08-23 21:40:01 -0300

    feat: navegação pelas páginas

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 function Header() {
     return (
         <header className="header">
             <nav>
                 <ul>
-                    <li className="headerLinks">Home</li>
-                    <li className="headerLinks">Sobre</li>
-                    <li className="headerLinks">Contato</li>
+                    <li className="headerLinks"><Link to={"/"}>Home</Link></li>
+                    <li className="headerLinks"><Link to={"sobre"}>Sobre</Link></li>
+                    <li className="headerLinks"><Link to={"contato"}>Contato</Link></li>
                 </ul>
             </nav>
         </header>);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "sobre",
+        element: null
+      },
+      {
+        path: "contato",
+        element: null
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
